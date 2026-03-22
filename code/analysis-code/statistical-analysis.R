b6927050e0fce57aa30fa5da1354b644f0e2bd11
LiangLai <lrealist3@gmail.com>
2026-03-22 00:58:32 -0400

    part 3 to fit model

--- code/analysis-code/statistical-analysis.R
+++ code/analysis-code/statistical-analysis.R
@@ -8,13 +8,16 @@
 library(ggplot2) #for plotting
 library(broom) #for cleaning up output from lm()
 library(here) #for data loading/saving
-
+library(tidyverse)
+library(haven) 
+library(stringr)
+library(zoo)
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","processed-data","processeddata.rds")
+data_location <- here::here("data","processed-data","hrs_final_long.rds")
 
 #load data. 
-mydata <- readRDS(data_location)
+hrs_final <- readRDS(data_location)
 
 
 ######################################
@@ -24,33 +27,63 @@ mydata <- readRDS(data_location)
 ############################
 #### First model fit
 # fit linear model using height as outcome, weight as predictor
+# Define eligible risk set for W3
+eligible_w3 <- hrs_final %>%
+  filter(STUDYYR %in% c(1996, 1998, 2000)) %>%
+  group_by(HHIDPN) %>%
+  summarise(
+    n_waves_present   = n_distinct(STUDYYR),
+    any_cog_imp_to_lm = any(COGFUNCTION %in% c("CIND", "Demented"),
+                             na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  filter(
+    n_waves_present == 3,   # Present in all 3 waves (1996, 1998, 2000)
+    !any_cog_imp_to_lm      # No CIND or Dementia up to landmark
+  ) %>%
+  pull(HHIDPN)
 
-lmfit1 <- lm(Height ~ Weight, mydata)  
+cat("W3 eligible individuals:", length(eligible_w3), "\n")
 
-# place results from fit into a data frame with the tidy function
-lmtable1 <- broom::tidy(lmfit1)
+# Step 2: Build long survival data after landmark (2000)
+surv_w3 <- hrs_final %>%
+  filter(HHIDPN %in% eligible_w3,
+         STUDYYR >= 2000) %>%
+  arrange(HHIDPN, STUDYYR) %>%
+  group_by(HHIDPN) %>%
+  mutate(
+    tstart         = lag(STUDYYR, default = 2000),
+    tstop          = STUDYYR,
+    event_dementia = as.integer(COGFUNCTION == "Demented")
+  ) %>%
+  filter(tstart < tstop) %>%
+  # Truncate rows after first dementia event
+  filter(cumsum(lag(event_dementia, default = 0)) == 0) %>%
+  ungroup()
 
-#look at fit results
-print(lmtable1)
+cat("W3 survival data dimensions:", dim(surv_w3), "\n")
 
-# save fit results table  
-table_file1 = here("results", "tables", "resulttable1.rds")
-saveRDS(lmtable1, file = table_file1)
+surv_w3 %>%
+  summarise(
+    n_people   = n_distinct(HHIDPN),
+    n_rows     = n(),
+    n_events   = n_distinct(HHIDPN[event_dementia == 1]),
+    event_rate = round(n_distinct(HHIDPN[event_dementia == 1]) / 
+                       n_distinct(HHIDPN) * 100, 1)
+  ) %>%
+  print()
 
-############################
-#### Second model fit
-# fit linear model using height as outcome, weight and gender as predictor
+#  Cox model
+library(survival)
 
-lmfit2 <- lm(Height ~ Weight + Gender, mydata)  
-
-# place results from fit into a data frame with the tidy function
-lmtable2 <- broom::tidy(lmfit2)
-
-#look at fit results
-print(lmtable2)
-
-# save fit results table  
-table_file2 = here("results", "tables", "resulttable2.rds")
-saveRDS(lmtable2, file = table_file2)
-
+fit_w3 <- coxph(
+  Surv(tstart, tstop, event_dementia) ~
+    BMI_CV_W3 +
+    BASELINE_AGE + RAGENDER + RACE_ETH3 + RAEDUC +
+    LN_WEALTH + CHRONIC_N + HAS_INS +
+    EMP_STATUS + SMOKESTATUS + R_DEPRES +
+    cluster(HHIDPN),
+  data = surv_w3
+)
+summary(fit_w3)
   
